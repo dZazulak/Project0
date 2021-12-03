76d9b2f4c0b5966636887699b5be67307dbabe95
dZazulak <david.zazulak@revature.net>
2021-12-03 16:28:41 -0600

    Added tests for account service postgres

--- project0/tests_for_app/postgres_tests/service_tests/test_account_service_postgres.py
+++ project0/tests_for_app/postgres_tests/service_tests/test_account_service_postgres.py
@@ -1,5 +1,7 @@
 from custom_exceptions.account_not_found_exception import AccountNotFoundException
+from custom_exceptions.customer_not_found_exception import CustomerNotFoundException
 from custom_exceptions.duplicate_account_id_exception import DuplicateAccountIdException
+from custom_exceptions.insufficient_funds_exception import InsufficientFundsException
 from data_access_layer.implementation_classes.account_postgres_dao import AccountPostgresDAO
 from entities.account import Account
 from service_layer.implementation_services.account_postgres_service import AccountPostgresService
@@ -8,6 +10,10 @@ account_dao = AccountPostgresDAO()
 account_service = AccountPostgresService(account_dao)
 
 account = Account(500, 2, 1)
+deposit_account_account_id_fail = Account(500, 1, 1)
+withdraw_account_account_id_fail = Account(500, 1, 17)
+withdraw_account_customer_id_fail = Account(500, 3, 9999)
+withdraw_account_funds_fail = Account(999999999, 3, 1)
 
 
 def test_validate_create_account_method():
@@ -18,14 +24,31 @@ def test_validate_create_account_method():
         assert str(e) == "This account ID is already created."
 
 
-def test_validate_get_account_by_id_method():
+def test_validate_deposit_into_account_by_id_method_account_id_fail():
     try:
-        account_service.service_get_account_by_id(account)
+        account_service.service_deposit_into_account_by_id(deposit_account_account_id_fail)
         assert False
     except AccountNotFoundException as e:
         assert str(e) == "This account could not be found in the database"
 
 
-def test_validate_get_all_accounts_method():
-    result = account_service.service_get_all_accounts()
-    assert len(result) >= 2
+def test_validate_withdraw_from_account_by_id_method_account_id_fail():
+    try:
+        account_service.service_withdraw_from_account_by_id(withdraw_account_account_id_fail)
+        assert False
+    except AccountNotFoundException as e:
+        assert str(e) == "This account could not be found in the database"
+
+
+def test_validate_withdraw_from_account_by_id_method_customer_id_fail():
+    try:
+        account_service.service_withdraw_from_account_by_id(withdraw_account_customer_id_fail)
+    except CustomerNotFoundException as e:
+        assert str(e) == "This customer could not be found in the database"
+
+
+def test_validate_withdraw_from_account_by_id_method_funds_fail():
+    try:
+        account_service.service_withdraw_from_account_by_id(withdraw_account_funds_fail)
+    except InsufficientFundsException as e:
+        assert str(e) == "You do not have enough money in your account"
